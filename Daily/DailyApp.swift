05e5e67aa09eb229da33f68836fecfad69d346ef
michael-bautista <mrbautistadev@gmail.com>
2025-04-09 13:50:51 -0700

    Initial Commit

---
+++ Daily/DailyApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyApp.swift
+//  Daily
+//
+//  Created by Michael Bautista on 4/9/25.
+//
+
+import SwiftUI
+
+@main
+struct DailyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DailyTests/DailyTests.swift
@@ -0,0 +1,17 @@
+//
+//  DailyTests.swift
+//  DailyTests
+//
+//  Created by Michael Bautista on 4/9/25.
+//
+
+import Testing
+@testable import Daily
+
+struct DailyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
